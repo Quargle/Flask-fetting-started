bb6508b72757112916a093a454a7a02be1a0f8b1
Peter Evans <peter.evans@cerberusnuclear.com>
2020-03-27 17:12:08 +0000

    Improvements to cards and welcome
    Signed-off-by: Peter Evans <peter.evans@cerberusnuclear.com>

--- flashcards.py
+++ flashcards.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, abort
 from model import db
 
 app = Flask(__name__)   # in this case, passes "flashcards"
@@ -8,11 +8,18 @@ app = Flask(__name__)   # in this case, passes "flashcards"
 def welcome():
     return render_template(
         "welcome.html",
-        message="Here's a message from the view function."
-    )
+        cards=db,
+        )
 
 
-@app.route("/card")
-def card_view():
-    card = db[0]
-    return render_template("card.html", card=card)
+@app.route("/card/<int:index>")
+def card_view(index):
+    try:
+        card = db[index]
+        return render_template("card.html",
+                               card=card,
+                               index=index,
+                               max_index=len(db)-1,
+                               )
+    except IndexError:
+        abort(404)
