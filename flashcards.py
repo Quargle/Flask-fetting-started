616ffdae5282603f1ef3e7b0a1ce0fe103f10e05
Peter Evans <peter.evans@cerberusnuclear.com>
2020-03-27 16:21:46 +0000

    Model added
    Signed-off-by: Peter Evans <peter.evans@cerberusnuclear.com>

--- flashcards.py
+++ flashcards.py
@@ -1,12 +1,18 @@
 from flask import Flask, render_template
-
+from model import db
 
 app = Flask(__name__)   # in this case, passes "flashcards"
 
 
 @app.route("/")
 def welcome():
-    return render_template("welcome.html")
+    return render_template(
+        "welcome.html",
+        message="Here's a message from the view function."
+    )
 
 
-
+@app.route("/card")
+def card_view():
+    card = db[0]
+    return render_template("card.html", card=card)
